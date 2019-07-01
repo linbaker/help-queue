751662afba5fa4a32be6d89f6fe5cc0a43218e9d
brnhensley@gmail.com and Lindsey Baker <lindsey@>
2019-07-01 09:22:27 -0700

    Create ticket queue file structure

---
+++ src/components/TicketList.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Ticket from './Ticket';
+
+function TicketList(){
+    return (
+        <Ticket
+        location="2C"
+        names="Brian and Lindsey"
+        issue="React is like wicked lame"/>
+    );
+}
+
+export default TicketList;
